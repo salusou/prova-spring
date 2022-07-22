1cb6c7165f556008f74ad534778b6b280458863a
Samuel Souza <samuluc@gmail.com>
2022-07-22 16:23:00 -0300

    Removendo o Mapper e lombok

--- src/main/java/com/ucsbr/com/prova/entity/User.java
+++ src/main/java/com/ucsbr/com/prova/entity/User.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity(name = "usuario")
-@Data
 public class User implements Serializable {
 
   private static final Long serialVersionUID = 1L;
@@ -28,4 +27,44 @@ public class User implements Serializable {
 
   @Column(nullable = false)
   private String password;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
--- src/main/java/com/ucsbr/com/prova/service/dto/UserDTO.java
+++ src/main/java/com/ucsbr/com/prova/service/dto/UserDTO.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Data
 public class UserDTO implements Serializable {
 
   private static final Long serialVersionUID = 1L;
@@ -32,4 +31,43 @@ public class UserDTO implements Serializable {
   @NotNull
   private String password;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
